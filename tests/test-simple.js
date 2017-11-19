9feeab91dc1cfbb5a890d6da699ec6e4689e0906
Rob Hoelz <rob@hoelz.ro>
2017-11-19 10:51:35 -0600

    Fix up tests for async usage
    DON'T PUSH - split me up!
    
    We need to emulate the plugin-loading part of the boot process

--- tests/test-simple.js
+++ tests/test-simple.js
@@ -5,16 +5,49 @@ tags: [[$:/tags/test-spec]]
 
 \*/
 (function() {
+    var wiki = new $tw.Wiki();
+    wiki.addTiddler($tw.wiki.getTiddler('$:/plugins/hoelzro/progress-bar'));
+    wiki.addTiddler($tw.wiki.getTiddler('$:/plugins/hoelzro/full-text-search'));
+    wiki.readPluginInfo();
+    wiki.registerPluginTiddlers('plugin');
+    wiki.unpackPluginTiddlers();
+
+    wiki.addTiddler({
+        title: 'NoModified',
+        text: 'No modification date'
+    });
+
     describe('Simple test', function() {
-        var wiki = new $tw.Wiki();
-        wiki.addTiddler({
-            title: 'NoModified',
-            text: 'No modification date'
+        it('should start with an uninitialized FTS state', function() {
+            expect(wiki.getTiddlerText('$:/temp/FTS-state')).toBe('uninitialized');
         });
 
-        it('should find tiddlers without a modified field', function() {
-            var results = wiki.compileFilter('[ftsearch[modification]]')();
-            expect(results).toContain('NoModified');
+        var FTSActionGenerateIndexWidget = require('$:/plugins/hoelzro/full-text-search/fts-action-generate-index.js')['fts-action-generate-index'];
+        var widget = new FTSActionGenerateIndexWidget(null, {
+            wiki: wiki
+        });
+
+        it('should find matching documents without a modified field', function() {
+            var finished = false;
+
+            runs(function() {
+                widget.asyncInvokeAction().then(function() {
+                    finished = true;
+                }, function(err) {
+                    // XXX signal to jasmine that we failed?
+                    console.error(err);
+                });
+            });
+
+            waitsFor(function() {
+                return finished;
+            });
+
+            runs(function() {
+                expect(wiki.getTiddlerText('$:/temp/FTS-state')).toBe('initialized');
+                var results = wiki.compileFilter('[ftsearch[modification]]')();
+                expect(results).toContain('NoModified');
+            });
         });
     });
 })();
