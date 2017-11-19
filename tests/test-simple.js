eda6cefb0027c89c0b129f738f66d38fed4e9716
Rob Hoelz <rob@hoelz.ro>
2017-11-19 11:02:57 -0600

    Add test for changing a tiddler's contents

--- tests/test-simple.js
+++ tests/test-simple.js
@@ -49,5 +49,38 @@ tags: [[$:/tags/test-spec]]
                 expect(results).toContain('NoModified');
             });
         });
+
+        it("should pick up changes to tiddlers' contents", function() {
+            var finished = false;
+
+            runs(function() {
+                widget.asyncInvokeAction().then(function() {
+                    finished = true;
+                }, function(err) {
+                    // XXX signal to jasmine that we failed?
+                    console.error(err);
+                });
+            });
+
+            waitsFor(function() {
+                return finished;
+            });
+
+            runs(function() {
+                var tiddler = wiki.getTiddler('NoModified');
+                var newTiddler = new $tw.Tiddler(
+                    tiddler,
+                    {text: "New text without that word we're looking for"},
+                    wiki.getModificationFields());
+                wiki.addTiddler(newTiddler);
+                $tw.hooks.invokeHook('th-saving-tiddler', newTiddler);
+
+                var results = wiki.compileFilter('[ftsearch[modification]]')();
+                expect(results).not.toContain('NoModified');
+
+                results = wiki.compileFilter('[ftsearch[looking]]')();
+                expect(results).toContain('NoModified');
+            });
+        });
     });
 })();
