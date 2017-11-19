7f449cb4f4fdf88f2213e166df28b41140e0a4ef
Rob Hoelz <rob@hoelz.ro>
2017-11-19 11:23:42 -0600

    Encapsulate hook setup in a proper startup function
    TiddlyWiki startup modules are supposed to export a startup function,
    which is then called when appropriate, rather than what I was doing,
    which was just running my startup code upon load

--- hooks.ts
+++ hooks.ts
@@ -8,16 +8,20 @@ module-type: startup
 declare var $tw;
 
 module SaveTiddlerHook {
-    var getIndex = require('$:/plugins/hoelzro/full-text-search/shared-index.js').getIndex;
+    export function startup() {
+        var getIndex = require('$:/plugins/hoelzro/full-text-search/shared-index.js').getIndex;
 
-    $tw.hooks.addHook('th-saving-tiddler', function(tiddler) {
-        getIndex().update(tiddler.fields);
-        return tiddler;
-    });
+        $tw.hooks.addHook('th-saving-tiddler', function(tiddler) {
+            getIndex().update(tiddler.fields);
+            return tiddler;
+        });
 
-    $tw.hooks.addHook('th-deleting-tiddler', function(tiddler) {
-        getIndex().remove({ title: tiddler.fields.title });
-    });
+        $tw.hooks.addHook('th-deleting-tiddler', function(tiddler) {
+            getIndex().remove({ title: tiddler.fields.title });
+        });
+    }
 }
+
+export = SaveTiddlerHook;
 
 // vim:sts=4:sw=4
