9116561021be69ad3d327c030d6bc714978e3c9a
Rob Hoelz <rob@hoelz.ro>
2017-11-19 12:25:09 -0600

    Stop invoking hooks to test index updates
    Instead, just wait for the event queue to be cleared via
    $tw.utils.nextTick

--- tests/test-simple.js
+++ tests/test-simple.js
@@ -50,7 +50,15 @@ tags: [[$:/tags/test-spec]]
 
             runs(function() {
                 widget.asyncInvokeAction().then(function() {
-                    finished = true;
+                    var tiddler = wiki.getTiddler('NoModified');
+                    var newTiddler = new $tw.Tiddler(
+                        tiddler,
+                        {text: "New text without that word we're looking for"},
+                        wiki.getModificationFields());
+                    wiki.addTiddler(newTiddler);
+                    $tw.utils.nextTick(function() {
+                        finished = true;
+                    });
                 }, function(err) {
                     // XXX signal to jasmine that we failed?
                     console.error(err);
@@ -62,14 +70,6 @@ tags: [[$:/tags/test-spec]]
             });
 
             runs(function() {
-                var tiddler = wiki.getTiddler('NoModified');
-                var newTiddler = new $tw.Tiddler(
-                    tiddler,
-                    {text: "New text without that word we're looking for"},
-                    wiki.getModificationFields());
-                wiki.addTiddler(newTiddler);
-                $tw.hooks.invokeHook('th-saving-tiddler', newTiddler);
-
                 var results = wiki.compileFilter('[ftsearch[modification]]')();
                 expect(results).not.toContain('NoModified');
 
