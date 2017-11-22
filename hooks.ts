4a499d79f1665e783feffa1867ba2e4ae8693526
Rob Hoelz <rob@hoelz.ro>
2017-11-22 16:27:59 -0600

    Invalidate index if related terms config changes
    Otherwise the user might be given results inconsistent with their
    configuration change

--- hooks.ts
+++ hooks.ts
@@ -8,8 +8,11 @@ module-type: startup
 declare var $tw;
 
 module SaveTiddlerHook {
+    const RELATED_TERMS_TIDDLER = '$:/plugins/hoelzro/full-text-search/RelatedTerms.json';
+    const STATE_TIDDLER = '$:/temp/FTS-state';
+
     export function startup() {
-        var { updateTiddler, getIndex } = require('$:/plugins/hoelzro/full-text-search/shared-index.js');
+        var { updateTiddler, getIndex, clearIndex } = require('$:/plugins/hoelzro/full-text-search/shared-index.js');
 
         $tw.wiki.addEventListener('change', function(changes) {
             let index = getIndex();
@@ -18,6 +21,15 @@ module SaveTiddlerHook {
             }
 
             for(var title in changes) {
+                if(title == RELATED_TERMS_TIDDLER) {
+                    clearIndex();
+                    let stateTiddler = $tw.wiki.getTiddler(STATE_TIDDLER);
+                    $tw.wiki.addTiddler(new $tw.Tiddler(
+                        stateTiddler,
+                        { text: 'uninitialized' },
+                       $tw.wiki.getModificationFields()));
+                }
+
                 if($tw.wiki.isSystemTiddler(title)) {
                     continue;
                 }
