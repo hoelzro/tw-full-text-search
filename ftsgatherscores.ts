2634f33648112be0b3d525a239ba841402c3d920
Rob Hoelz <rob@hoelz.ro>
2018-10-13 12:47:51 -0500

    Normalize FTS scores
    lunr.js isn't supposed to return scores greater than 1, but for some
    reason it seems to do that.  Until I figure out why, let's normalize
    the scores to 0-1 so that I can work on my score displaying logic

--- ftsgatherscores.ts
+++ ftsgatherscores.ts
@@ -8,12 +8,21 @@ module-type: filteroperator
 module FTSGatherScores {
     export function ftsgatherscores(source, operator, options) {
         let targetTiddler = operator.operand;
+        let topScore = null;
         return function(callback) {
             let titleToScore = Object.create(null);
             source(function(tiddler, title, score) {
                 titleToScore[title] = score;
+                if(topScore === null || score > topScore) {
+                    topScore = score;
+                }
                 callback(tiddler, title);
             });
+            if(topScore > 1.0) {
+                for(let title in titleToScore) {
+                    titleToScore[title] /= topScore;
+                }
+            }
             options.wiki.setTiddlerData(targetTiddler, titleToScore);
         };
     }
