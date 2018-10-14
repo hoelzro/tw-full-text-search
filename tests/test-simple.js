ec3c28207d64c95868eec6189d9e7f34f27fd0a5
Rob Hoelz <rob@hoelz.ro>
2018-10-14 10:03:41 -0500

    Revert query relevance display work
    This reverts the following commits:
    
      * 0530f35
      * 85c4ed2
      * f515f0f
      * d33f427
      * 2634f33
      * 5f7ce19
      * 4eb6a9c
      * ef0bc23
      * ad23357
      * 1b7f153
      * 7c78ebd
      * 1685d8a
    
    See GH #16 for reason why the work was done in the first place
    
    I'm reverting this because it's painfully slow right now, and it's going
    to take a good amount of work to make it fast, if it's at all possible.
    This feature wasn't originally slated for the 1.1.0 release, so I'm
    pulling out all of the code I wrote for it in order to make 1.1.0
    happen; afterwards I can unrevert it when I'm ready to commit to adding
    the feature in a future release

--- tests/test-simple.js
+++ tests/test-simple.js
@@ -385,26 +385,6 @@ https://jaredforsyth.com/2017/07/05/a-reason-react-tutorial/
                 expect(foxesFoxesFoxesIndex).toBeLessThan(foxInGardenIndex);
             });
         });
-
-        it('should gather scoring information into a tiddler if asked', function() {
-            prepare().then(function() {
-                let results = wiki.filterTiddlers('[ftsearch[fox]ftsgatherscores[$:/temp/score-data]]');
-                let scoringData = wiki.getTiddlerData('$:/temp/score-data');
-                expect('Foxes foxes foxes' in scoringData).toBeTruthy();
-                expect('A fox in the garden' in scoringData).toBeTruthy();
-            });
-        });
-
-        it('should never have scores that exceed 1.0', function() {
-            prepare().then(function() {
-                let results = wiki.filterTiddlers('[ftsearch[fox]ftsgatherscores[$:/temp/score-data]]');
-                let scoringData = wiki.getTiddlerData('$:/temp/score-data');
-
-                for(let score of Object.values(scoringData)) {
-                    expect(score).not.toBeGreaterThan(1.0);
-                }
-            });
-        });
     });
 
     describe('Cache tests', function() {
