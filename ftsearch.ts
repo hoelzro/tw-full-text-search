77ebabc558b2a91deef9cbd2ce01ffb663408bcd
Rob Hoelz <rob@hoelz.ro>
2018-11-24 16:43:54 -0600

    Detect fuzzy searches if fuzzy searching is disabled
    ...and pass data down to ftsfeedback so we can notify the user

--- ftsearch.ts
+++ ftsearch.ts
@@ -8,6 +8,7 @@ module-type: filteroperator
 declare var require;
 
 module FTSearch {
+    const FUZZY_SEARCH_TIDDLER = '$:/plugins/hoelzro/full-text-search/EnableFuzzySearching';
     var lunr = require('$:/plugins/hoelzro/full-text-search/lunr.min.js');
     var getIndex = require('$:/plugins/hoelzro/full-text-search/shared-index.js').getIndex;
 
@@ -22,9 +23,22 @@ module FTSearch {
             return [];
         }
         try {
-            var results = index.search(operator.operand);
-
             return function(callback) {
+                let fuzzySearchesEnabled = options.wiki.getTiddlerText(FUZZY_SEARCH_TIDDLER, '') == 'yes';
+                if(!fuzzySearchesEnabled) {
+                    let qp = new lunr.QueryParser(operator.operand, new lunr.Query(['title', 'tags', 'text']));
+                    let query = qp.parse();
+                    for(let clause of query.clauses) {
+                        if(!clause.usePipeline) {
+                            // we're using a wildcard, but the index isn't prepared for
+                            // fuzzy searches - so pass information on this down the pipeline
+                            return callback(null, null, "It looks like you're trying to perform a fuzzy search; you'll need to enable fuzzy searching in the FTS settings");
+                        }
+                    }
+                }
+
+                var results = index.search(operator.operand);
+
                 for(let match of results) {
                     if(match.ref in sourceLookup) {
                         callback(sourceLookup[match.ref], match.ref);
