c71378c9aca03d9f703d515c8a68c932d671983b
Rob Hoelz <rob@hoelz.ro>
2018-11-24 19:59:11 -0600

    Fix up detection of fuzzy/wildcard features

--- ftsearch.ts
+++ ftsearch.ts
@@ -35,7 +35,12 @@ module FTSearch {
                         if(!clause.usePipeline) {
                             // we're using a wildcard, but the index isn't prepared for
                             // fuzzy searches - so pass information on this down the pipeline
-                            return callback(null, null, "It looks like you're trying to perform a fuzzy search; you'll need to enable fuzzy searching in the FTS settings");
+                            return callback(null, null, "It looks like you're trying to perform a wildcard search; you'll need to enable wildcard/fuzzy searching in the FTS settings");
+                        }
+                        if('editDistance' in clause) {
+                            // we're using a fuzzy search, but the index isn't prepared for
+                            // fuzzy searches - so pass information on this down the pipeline
+                            return callback(null, null, "It looks like you're trying to perform a fuzzy search; you'll need to enable wildcard/fuzzy searching in the FTS settings");
                         }
                     }
                 }
